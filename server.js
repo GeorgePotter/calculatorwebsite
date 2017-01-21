cb21f15e44d78e28769abcabe61420568a88013b
GeorgePotter <wpotter.george@gmail.com>
2017-01-21 01:23:19 +0000

    fiddling

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Tax Bombshell Calculator</h1>');
+    res.end('<h1>Tax Bombshell Calculator a</h1>'
+    );
     var msg = "Tories suck"
 res.end(<h2>msg</h2>)
 }).listen(process.env.PORT);
