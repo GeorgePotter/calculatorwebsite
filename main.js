d46c7ec26b82e37c8db933c13917a1de60efcfd2
GeorgePotter <wpotter.george@gmail.com>
2017-01-21 01:31:50 +0000

    tutorial

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
+
     res.end('<h1>Tax Bombshell Calculator a</h1>'
     );
 }).listen(process.env.PORT);
+
+var fs = require("fs");
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+
+console.log("Program Ended");
